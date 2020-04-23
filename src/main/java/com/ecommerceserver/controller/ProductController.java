565eb099683c1ca1493591fa088eb48a82b230ee
NHD98 <ndreece1998@gmail.com>
2020-04-23 11:04:36 +0700

    fix ReviewStar and particle

--- src/main/java/com/ecommerceserver/controller/ProductController.java
+++ src/main/java/com/ecommerceserver/controller/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import com.ecommerceserver.model.Category;
 import com.ecommerceserver.model.Product;
+import com.ecommerceserver.model.ReviewStar;
 import com.ecommerceserver.services.CategoryService;
 import com.ecommerceserver.services.ProductService;
 
@@ -34,7 +35,7 @@ public class ProductController {
   @Autowired
   CategoryService categoryService;
 
-  @GetMapping("/product/{id}")
+  @GetMapping("/{id}")
   public ResponseEntity<?> getProductById(@PathVariable String id) {
     Optional<Product> product = productService.findById(id);
     if (product.isPresent()) {
@@ -45,6 +46,10 @@ public class ProductController {
 
   @PostMapping
   public List<Product> addProduct(@RequestBody List<Product> lstProduct) {
+    for (Product product : lstProduct) {
+      ReviewStar reviewStar = new ReviewStar();
+      product.setReviewStar(reviewStar);
+    }
     return productService.addList(lstProduct);
   }
 
@@ -69,9 +74,9 @@ public class ProductController {
     Pageable pageable;
     String asc = "ascending";
     if (sortDirection.equals(asc)) {
-      pageable = PageRequest.of(page - 1, 16, Sort.by(Sort.Direction.ASC, sortBy));
+      pageable = PageRequest.of(page - 1, 15, Sort.by(Sort.Direction.ASC, sortBy));
     } else {
-      pageable = PageRequest.of(page - 1, 16, Sort.by(Sort.Direction.DESC, sortBy));
+      pageable = PageRequest.of(page - 1, 15, Sort.by(Sort.Direction.DESC, sortBy));
     }
     if (search.isEmpty()) {
       return productService.getProductByCategory(categoryId, pageable);
--- src/main/java/com/ecommerceserver/model/ReviewStar.java
+++ src/main/java/com/ecommerceserver/model/ReviewStar.java
@@ -1,5 +1,12 @@
 package com.ecommerceserver.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewStar {
   int _1star = 0;
   int _2star = 0;
